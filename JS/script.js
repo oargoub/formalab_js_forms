4f7ec7b153cfe2e62ab755e088f3f7dab7ea06d3
oargoub <oumaima.argoubi8@gmail.com>
2019-05-21 21:38:20 +0200

    js

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+alert ("bonjour");
+function message (){/* fonction nommée message qui permet d'afficher un message bonjour à tous*/
+
+    alert('bonjour à tous !!!');
+}
+function message2(t){
+
+    alert('bonjour '+t.value);/*t c'est le  var de stockage du contenu de paramétre document.form2.txtNom(html)*/
+}
+function carré(tnb)
+{
+    return tnb.value*tnb.value;
+}
